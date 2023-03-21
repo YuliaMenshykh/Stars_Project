826654088a001afd61c6265f798e36b6fb60ebc2
YuliaMenshykh <yulia.menshykh@gmail.com>
2023-03-21 18:08:45 +0200

    changed structure of folders

---
+++ InClass/Source.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+
+//
+//class A
+//{
+//public:
+//	int d;
+//
+//	~A()
+//	{
+//		std::cout << "1";
+//	}
+//};
+
+
+
+int main()
+{
+	/*size_t std = sizeof(int);
+	int* ptr = new int[100];
+
+	delete[] ptr;*/
+
+
+	// Work with class A
+	/*A* ptr2 = new A[100];
+	int* nCo = (int*)((char*)ptr2 - sizeof(void*));
+	std::cout << *nCo;
+	delete ptr2;*/
+
+
+
+
+
+}
